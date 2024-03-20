43b98ff2da4c9b3bb4885a1924cd94bb64d9c6e1
samxoan <164140639+samxoan@users.noreply.github.com>
2024-03-20 13:49:31 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("7tUCkRpY2AUER9TUd7Nb4LJW5wgCjZoKx2UG2uoQCjpK");
 
 #[program]
 pub mod nft_lend_borrow {
